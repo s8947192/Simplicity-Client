31151131ac72390ae771f35450a76bb93442d5fa
Eugene <frostberryart@gmail.com>
2018-04-20 22:25:38 +0300

    history fix

--- src/universal/sagas/auth.js
+++ src/universal/sagas/auth.js
@@ -1,5 +1,5 @@
 import { takeLatest, call, put } from 'redux-saga/effects'
-import { history } from 'client/client'
+import { push } from 'react-router-redux'
 
 import { authRequest, registrationRequest, logoutRequest } from 'api/auth'
 import { LOGIN, REGISTRATION, LOGOUT } from 'actions/auth'
@@ -10,7 +10,7 @@ function* requestRegistration({ email, password }) {
   try {
     yield put(registrationPending())
     const { data: user } = yield call(registrationRequest, email, password)
-    yield call(history.push, `/user/${user.id}`)
+    yield put(push(`/user/${user.id}`))
   } catch (e) {
     yield put(registrationRejected(e.response.data.error.details.messages.email[0]))
   }
@@ -22,7 +22,7 @@ function* requestLogin({ email, password }) {
     const user = yield call(authRequest, email, password)
     localStorage.setItem('token', user.token)
     yield put(loginFulfilled(user))
-    yield call(history.push, `/user/${user.userId}`)
+    yield put(push(`/user/${user.userId}`))
   } catch (e) {
     yield put(loginRejected(e.response.data.error.message))
   }
@@ -32,7 +32,7 @@ function* requestLogout() {
   try {
     yield call(logoutRequest)
     localStorage.removeItem('token')
-    yield call(history.push, `/login`)
+    yield put(push('/login'))
   } catch (e) {
     console.log({ e })
   }
